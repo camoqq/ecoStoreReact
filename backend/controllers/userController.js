e9847c290a64d4dfb2ced430f35b4eb4ad6086d2
Allison Serrano <allisonserrano171@yahoo.com>
2024-01-20 13:14:32 -0500

    added get all users route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -17,3 +17,17 @@ DELETE a user (by id)
 not using this anywhere yet, unless we want to have a user delete there own profile
 just testing for now
 */
+
+const getUsers = async (req, res) => {
+  try {
+    await User.find();
+    res.json();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+};
+
+module.exports = {
+  getUsers
+};
